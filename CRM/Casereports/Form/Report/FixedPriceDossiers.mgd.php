77360a5c446d3e74f3491d175b84219024412487
Jaap Jansma <jaap@edeveloper.nl>
2017-05-24 15:28:06 +0200

    Fixed issue #1578 rapport fixed price dossiers voor account managers.

---
+++ CRM/Casereports/Form/Report/FixedPriceDossiers.mgd.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Jaap Jansma (CiviCooP) <jaap.jansma@civicoop.org>
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ */
+return array (
+    0 =>
+        array (
+            'name' => 'CRM_Casereports_Form_Report_FixedPriceDossiers',
+            'entity' => 'ReportTemplate',
+            'params' =>
+                array (
+                    'version' => 3,
+                    'label' => 'Fixed price dossiers',
+                    'description' => 'Een overzicht van fixed price dossiers.',
+                    'class_name' => 'CRM_Casereports_Form_Report_FixedPriceDossiers',
+                    'report_url' => 'be.werkenmetzin.caseinvoice/fixedpriceadossiers',
+                    'component' => 'CiviCase',
+                ),
+        ),
+);
--- CRM/Casereports/Form/Report/FixedPriceDossiers.php
+++ CRM/Casereports/Form/Report/FixedPriceDossiers.php
@@ -32,26 +32,21 @@
  * $Id$
  *
  */
-class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM_Report_Form {
+class CRM_Casereports_Form_Report_FixedPriceDossiers extends CRM_Report_Form {
 
   protected $_summary = NULL;
   protected $_relField = FALSE;
   protected $_exposeContactID = FALSE;
 
-  protected $_customGroupExtends = array('Case', 'Activity');
+  protected $_customGroupExtends = array('Case');
 
   protected $_absoluteUrl = TRUE;
 
   protected $km;
 
   public function __construct() {
-    $this->km = CRM_Core_BAO_Setting::getItem('be.werkmetzin.caseinvoice', 'km', null, 0.4);
-    $kmCustomGroup = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'KM'));
-    $kmCustomField = civicrm_api3('CustomField', 'getsingle', array('name' => 'km', 'custom_group_id' => $kmCustomGroup['id']));
     $this->case_types = CRM_Case_PseudoConstant::caseType();
     $this->case_statuses = CRM_Core_OptionGroup::values('case_status');
-    $this->activity_statuses = CRM_Core_OptionGroup::values('activity_status');
-    $this->activity_type = CRM_Core_OptionGroup::values('activity_type');
     $rels = CRM_Core_PseudoConstant::relationshipType();
     foreach ($rels as $relid => $v) {
       $this->rel_types[$relid] = $v['label_b_a'];
@@ -95,6 +90,14 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
             'title' => ts('Case Subject'),
             'default' => TRUE,
           ),
+          'case_type_id' => array(
+            'title' => ts('Case type'),
+            'default' => true,
+          ),
+          'status_id' => array(
+            'title' => ts('Status'),
+            'default' => true,
+          ),
         ),
         'filters' => array(
           'case_type_id' => array(
@@ -103,7 +106,7 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
             'options' => $this->case_types,
           ),
           'status_id' => array(
-            'title' => ts('Case Status'),
+            'title' => ts('Status'),
             'operatorType' => CRM_Report_Form::OP_MULTISELECT,
             'options' => $this->case_statuses,
           ),
@@ -115,66 +118,6 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
             'default' => '1',
             'default_weight' => '1',
             'default_order' => 'ASC',
-            'default_is_section' => '1',
-          ),
-        ),
-      ),
-      'civicrm_subcase' => array(
-        'dao' => 'CRM_Case_DAO_Case',
-        'fields' => array(
-          'subcase_id' => array(
-            'name' => 'id',
-            'title' => ts('Case ID'),
-            'required' => TRUE,
-            'no_display' => TRUE,
-          ),
-          'subcase_subject' => array(
-            'name' => 'subject',
-            'title' => ts('Dossier onderwerp (subcase)'),
-            'default' => TRUE,
-          ),
-        ),
-      ),
-      'civicrm_activity' => array(
-        'fields' => array(
-          'activity_type_id' => array(
-            'title' => ts('Activity Type'),
-            'default' => TRUE,
-          ),
-          'activity_date_time' => array(
-            'title' => ts('Activity Date'),
-            'default' => true,
-          ),
-          'duration' => array(
-            'title' => ts('Duration'),
-            'default' => true,
-          ),
-        ),
-        'filters' => array(
-          'activity_type_id' => array(
-            'title' => ts('Activity Type'),
-            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
-            'options' => CRM_Core_PseudoConstant::activityType(TRUE, TRUE, FALSE, 'label', TRUE),
-          ),
-          'activity_status_id' => array(
-            'title' => ts('Status'),
-            'name' => 'status_id',
-            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
-            'options' => CRM_Core_PseudoConstant::activityStatus('label'),
-          ),
-          'activity_date_time' => array(
-            'title' => ts('Activity Date'),
-            'operatorType' => CRM_Report_Form::OP_DATE,
-            'type' => CRM_Utils_Type::T_DATE,
-          ),
-        ),
-        'order_bys' => array(
-          'activity_date_time' => array(
-            'name' => 'activity_date_time',
-            'title' => ts('Activity Date'),
-            'default' => '1',
-            'default_weight' => '2',
-            'default_order' => 'DESC',
             'default_is_section' => '0',
           ),
         ),
@@ -220,56 +163,33 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
       ),
     );
 
-    $this->_columns[$kmCustomGroup['table_name']] = array();
-
     parent::__construct();
-
-    $this->_columns[$kmCustomGroup['table_name']]['fields']['custom_'.$kmCustomField['id']]['default'] = TRUE;
-    unset($this->_columns[$kmCustomGroup['table_name']]['group_title']);
-    unset($this->_columns[$kmCustomGroup['table_name']]['grouping']);
   }
 
   public function select() {
     parent::select();
-
-    $activity = $this->_aliases['civicrm_activity'];
-    $this->_select .= ", invoice_settings.rate_coach AS invoice_settings_rate_coach, invoice_settings.rounding AS invoice_settings_rounding, {$activity}.duration AS activity_duration, km.km as activity_km";
   }
 
   public function from() {
-    $activity = $this->_aliases['civicrm_activity'];
     $case = $this->_aliases['civicrm_case'];
-    $subcase = $this->_aliases['civicrm_subcase'];
     $client = $this->_aliases['client'];
     $staff = $this->_aliases['staff'];
     $relationship = $this->_aliases['civicrm_relationship'];
     $relationship_type = $this->_aliases['civicrm_relationship_type'];
 
-    $this->_from = "FROM civicrm_activity {$activity} ";
-    $this->_from .= "INNER JOIN civicrm_case_activity ON civicrm_case_activity.activity_id = {$activity}.id ";
-    $this->_from .= "INNER JOIN civicrm_case {$subcase} ON civicrm_case_activity.case_id = {$subcase}.id ";
-    $this->_from .= "INNER JOIN civicrm_value_caselink_case ON civicrm_value_caselink_case.entity_id = {$subcase}.id ";
-    $this->_from .= "INNER JOIN civicrm_case {$case} ON  civicrm_value_caselink_case.case_id = {$case}.id ";
+
+    $this->_from = "FROM civicrm_case {$case} ";
     $this->_from .= "INNER JOIN civicrm_case_contact ON civicrm_case_contact.case_id = ${case}.id ";
     $this->_from .= "INNER JOIN civicrm_contact {$client} on {$client}.id = civicrm_case_contact.contact_id ";
     $this->_from .= "INNER JOIN civicrm_relationship {$relationship} ON {$relationship}.case_id = {$case}.id ";
     $this->_from .= "INNER JOIN civicrm_relationship_type {$relationship_type} ON {$relationship_type}.id = {$relationship}.relationship_type_id ";
     $this->_from .= "INNER JOIN civicrm_contact {$staff} ON {$staff}.id = {$relationship}.contact_id_b ";
-    $this->_from .= "LEFT JOIN civicrm_value_case_invoice_settings invoice_settings ON invoice_settings.entity_id = {$case}.id ";
-    $this->_from .= "LEFT JOIN civicrm_value_km km ON km.entity_id = {$activity}.id";
   }
 
   public function where() {
     parent::where();
 
-    $activity = $this->_aliases['civicrm_activity'];
-    $case = $this->_aliases['civicrm_case'];
-    $client = $this->_aliases['client'];
-    $staff = $this->_aliases['staff'];
     $relationship = $this->_aliases['civicrm_relationship'];
-    $relationship_type = $this->_aliases['civicrm_relationship_type'];
-
-    $this->_where .= " AND {$activity}.is_deleted = '0' AND {$activity}.is_current_revision = '1' AND {$case}.is_deleted = '0'";
     $this->_where .= "AND {$relationship}.is_active = '1' AND ({$relationship}.start_date IS NULL OR DATE({$relationship}.start_date) <= NOW()) AND ({$relationship}.end_date IS NULL OR DATE({$relationship}.end_date) >= NOW())";
     if (CRM_Utils_Array::value("my_cases_value", $this->_params)) {
       $session = CRM_Core_Session::singleton();
@@ -278,6 +198,9 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
   }
 
   public function modifyColumnHeaders() {
+    $this->_columnHeaders['manage_case'] = array(
+      'title' => '',
+    );
   }
 
   /**
@@ -295,25 +218,18 @@ class CRM_Casereports_Form_Report_FixedPriceActiviteitenSpecificatie extends CRM
       // convert Case ID and Subject to links to Manage Case
       if (array_key_exists('civicrm_case_subject', $row) && array_key_exists('civicrm_case_id', $row) && !empty($rows[$rowNum]['client_id'])) {
         $url = CRM_Utils_System::url("civicrm/contact/view/case", 'reset=1&action=view&cid=' . $row['client_id'] . '&id=' .$row['civicrm_case_id'],$this->_absoluteUrl);
-        $rows[$rowNum]['civicrm_case_subject_link'] = $url;
-        $rows[$rowNum]['civicrm_case_subject_hover'] = ts("Manage Case");
+        $rows[$rowNum]['manage_case'] = ts('Manage case');
+        $rows[$rowNum]['manage_case_link'] = $url;
+        $rows[$rowNum]['manage_case_hover'] = ts("Manage Case");
         $entryFound = TRUE;
       }
 
-      if (array_key_exists('civicrm_subcase_subcase_subject', $row) && array_key_exists('civicrm_subcase_subcase_id', $row) && !empty($rows[$rowNum]['client_id'])) {
-        $url = CRM_Utils_System::url("civicrm/contact/view/case", 'reset=1&action=view&cid=' . $row['client_id'] . '&id=' .$row['civicrm_subcase_subcase_id'],$this->_absoluteUrl);
-        $rows[$rowNum]['civicrm_subcase_subcase_subject_link'] = $url;
-        $rows[$rowNum]['civicrm_subcase_subcase_subject_hover'] = ts("Manage Case");
-        $entryFound = TRUE;
+      if (array_key_exists('civicrm_case_status_id', $row)) {
+        $rows[$rowNum]['civicrm_case_status_id'] = $this->case_statuses[$row['civicrm_case_status_id']];
       }
 
-      if (array_key_exists('civicrm_activity_activity_type_id', $row)) {
-        $type = '';
-        if (isset($this->activity_type[$row['civicrm_activity_activity_type_id']])) {
-          $type = $this->activity_type[$row['civicrm_activity_activity_type_id']];
-        }
-        $rows[$rowNum]['civicrm_activity_activity_type_id'] = $type;
-        $entryFound = TRUE;
+      if (array_key_exists('civicrm_case_case_type_id', $row)) {
+        $rows[$rowNum]['civicrm_case_case_type_id'] = $this->case_types[$row['civicrm_case_case_type_id']];
       }
 
       if (array_key_exists('client_client_name', $row) && !empty($rows[$rowNum]['client_id'])) {
