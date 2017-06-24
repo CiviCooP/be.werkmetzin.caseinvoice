9ed6637887d27447e785977a2fbf9803d113ca9c
Jaap Jansma <jaap@edeveloper.nl>
2017-06-24 11:25:09 +0200

    Fixed user story #1685: stap 2 vastleggen gegevens tbv facturatie door coach

--- CRM/Caseinvoice/Upgrader.php
+++ CRM/Caseinvoice/Upgrader.php
@@ -45,6 +45,48 @@ class CRM_Caseinvoice_Upgrader extends CRM_Caseinvoice_Upgrader_Base {
     return true;
   }
 
+	public function upgrade_1004() {
+		$this->executeCustomDataFile('xml/coach_invoice_settings.xml');
+		CRM_Core_DAO::executeQuery("INSERT INTO civicrm_value_coach_invoice_settings (`entity_id`, `rate_coach`) SELECT `entity_id`, `rate_coach` FROM `civicrm_value_case_invoice_settings`");
+
+		$result = civicrm_api3('OptionValue', 'create', array(
+			'name' => 'factureren_aanbreng_fee',
+			'label' => 'Aanbreng Fee factureren',
+			'option_group_id' => 'activity_type',
+			'component_id' => 7, // CiviCase
+		));
+		$factureren_aanbreng_fee = civicrm_api3('OptionValue', 'getsingle', array('id' => $result['id']));
+
+		$result = civicrm_api3('OptionValue', 'create', array(
+			'name' => 'factureren_outplacement_fee',
+			'label' => 'Outplacement Fee factureren',
+			'option_group_id' => 'activity_type',
+			'component_id' => 7, // CiviCase
+		));
+		$factureren_outplacement_fee = civicrm_api3('OptionValue', 'getsingle', array('id' => $result['id']));
+
+
+		$coaching_bedrijven_dossier = civicrm_api3('CaseType', 'getsingle', array('name' => 'coaching_voor_bedrijven'));
+		$coaching_bedrijven_dossier['definition']['activityTypes'][] = array('name' => $factureren_aanbreng_fee['name']);
+		$coaching_bedrijven_dossier['definition']['activityTypes'][] = array('name' => $factureren_outplacement_fee['name']);
+		civicrm_api3('CaseType', 'create', $coaching_bedrijven_dossier);
+
+		$coachingstraject = civicrm_api3('CaseType', 'getsingle', array('name' => 'coachingstraject'));
+		$coachingstraject['definition']['activityTypes'][] = array('name' => $factureren_outplacement_fee['name']);
+		civicrm_api3('CaseType', 'create', $coachingstraject);
+
+		$coachingstraject_io = civicrm_api3('CaseType', 'getsingle', array('name' => 'coachingstraject_io'));
+		$coachingstraject_io['definition']['activityTypes'][] = array('name' => $factureren_outplacement_fee['name']);
+		civicrm_api3('CaseType', 'create', $coachingstraject_io);
+
+		return true;
+	}
+
+	public function upgrade_1005() {
+		$this->executeCustomDataFile('xml/factureren_fee.xml');
+		return true;
+	}
+
   public function uninstall() {
     try {
       $case_info_settings_gid = civicrm_api3('CustomGroup', 'getvalue', array('name' => 'case_invoice_settings'));
